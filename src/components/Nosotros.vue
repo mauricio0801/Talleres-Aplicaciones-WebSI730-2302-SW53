61a633e4996706780fd751ba2b7bc1f99874f1b0
Erick130601 <104027427+Erick130601@users.noreply.github.com>
2023-08-29 22:17:04 -0500

    update

---
+++ src/components/Nosotros.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    Nosotros{{name}}
+  </div>
+
+</template>
+
+
+<script >
+export default {
+  props:{
+    name:String,
+  },
+  name:"Nosotros",
+}
+</script>
+
+
+<style scoped>
+
+.container{
+  margin: 2px;
+  background-color: black;
+  font-size: 50px;
+  color: #f2f2f2;
+  padding: 6px;
+  text-align: center;
+
+}
+</style>
