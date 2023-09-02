364265b60dd651d39081678bf1662713a1f36ebd
Mauricio Salas <119011860+mauricio0801@users.noreply.github.com>
2023-09-01 20:30:46 -0500

    Ajuste

---
+++ src/components/Contactanos.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    Contactanos
+
+    <button @click="handleClick" > Clickea aqui</button>
+  </div>
+</template>
+
+
+<script >
+export default {
+  props: {
+    name: String,
+  },
+  name: "Contactanos",
+  methods: {
+    handleClick(){
+      alert('992928323')
+    }
+  }
+
+}
+</script>
+
+
+<style scoped>
+.container {
+  margin: 2px;
+  background-color: black;
+  font-size: 50px;
+  color: #f2f2f2;
+  padding: 6px;
+  text-align: center;
+  border-radius: 30px;
+  font-family: sans-serif;
+
+}
+.container:hover{
+  background-color: red;
+
+}
+</style>
